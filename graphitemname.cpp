7890356435d45209800337f066124b86e4d9b68b
Leonid <leosko94@mail.ru>
2013-07-31 06:52:08 +0400

    Delete graphitemname.cpp
    It is not needed here really.

--- graphitemname.cpp
+++
@@ -1,9 +0,0 @@
-#include "graphitemname.h"
-
-GraphItemName::GraphItemName(QString name, QPointF position)
-{
-	setFlag(ItemIsMovable);
-	setFlag(ItemSendsGeometryChanges);
-	setCacheMode(DeviceCoordinateCache);
-	setZValue(-1);
-}
