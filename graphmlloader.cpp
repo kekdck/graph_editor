65ae41549cc8bc7b9761d141fe0fdb2448510ee2
zo0mer <exzo0mex@gmail.com>
2013-08-24 19:54:50 +0400

    Added function: save to GraphML file

---
+++ graphmlloader.cpp
@@ -0,0 +1,5 @@
+#include "graphmlloader.h"
+
+GraphmlLoader::GraphmlLoader()
+{
+}
---
+++ graphmlloader.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHMLLOADER_H
+#define GRAPHMLLOADER_H
+
+class GraphmlLoader
+{
+public:
+    GraphmlLoader();
+};
+
+#endif // GRAPHMLLOADER_H
